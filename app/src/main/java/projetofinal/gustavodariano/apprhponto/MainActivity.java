3a358f0c12b5cc570d2e670213be73fe322dfa00
Gustavo Dariano <gustavodariano@gmail.com>
2021-06-19 10:20:23 -0300

    Primeira Versão

---
+++ app/src/main/java/projetofinal/gustavodariano/apprhponto/MainActivity.java
@@ -0,0 +1,14 @@
+package projetofinal.gustavodariano.apprhponto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
